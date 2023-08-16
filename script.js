2aa8830239848fce84060722885732dac423a38c
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-08-16 12:50:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,91 @@
-// Fonction pour trier les données en ordre alphabétique
+// script.js
+
 function sortDataAlphabetically(data) {
   return data.sort((a, b) => a.abreviation.localeCompare(b.abreviation));
 }
 
-// Fonction pour gérer la recherche
+function displaySearchResults(results) {
+  const resultsList = document.getElementById("resultsList");
+  resultsList.innerHTML = "";
+  if (results.length === 0) {
+    resultsList.innerHTML = "<li>Aucun résultat trouvé</li>";
+    return;
+  }
+  
+  results.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${item.abreviation} - ${item.signification}`;
+    resultsList.appendChild(listItem);
+  });
+}
+
 function handleSearch(event, data) {
   const searchTerm = event.target.value.toLowerCase();
   const filteredResults = data.filter(item =>
     item.abreviation.toLowerCase().includes(searchTerm)
   );
-  displayResults(filteredResults);
+  displaySearchResults(filteredResults);
 }
 
-// Fonction pour afficher les résultats (filtrés ou non)
 function displayResults(results) {
-  const resultsList = document.getElementById("resultsList");
+  console.log("Displaying results:", results);
+  const resultsList = document.getElementById("resultsList"); // Correction de l'ID
   resultsList.innerHTML = '';
 
   if (results.length === 0) {
     resultsList.innerHTML = "<li>Aucun résultat trouvé</li>";
     return;
   }
-
+  
   results.forEach(result => {
-    const row = document.createElement("li");
-    const abbrCell = document.createElement("abbr");
+    const row = document.createElement("li"); // Créer une ligne de tableau (élément li)
+    
+    const abbrCell = document.createElement("abbr"); // Créer une cellule pour l'abréviation
     abbrCell.textContent = result.abreviation;
-    row.appendChild(abbrCell);
-
-    const descriptionCell = document.createElement("p");
+    row.appendChild(abbrCell); // Ajouter la cellule à la ligne
+    
+    const descriptionCell = document.createElement("p"); // Créer une cellule pour la description
     descriptionCell.textContent = result.signification;
-    row.appendChild(descriptionCell);
-
-    resultsList.appendChild(row);
+    row.appendChild(descriptionCell); // Ajouter la cellule à la ligne
+    
+    resultsList.appendChild(row); // Ajouter la ligne au tableau de résultats
   });
 }
 
-// Écouteur d'événement pour la barre de recherche
 const searchInput = document.getElementById("searchInput");
 
-// Fetch data and set up letter buttons
 fetch("data.json")
   .then(response => response.json())
   .then(data => {
     const sortedData = sortDataAlphabetically(data);
-    const letterButtons = document.querySelectorAll(".letter-button");
+    displaySearchResults(sortedData);
+    searchInput.addEventListener("input", event => handleSearch(event, sortedData));
 
+    const letterButtons = document.querySelectorAll(".letter-button");
+    
     letterButtons.forEach(button => {
       button.addEventListener("click", () => {
         const selectedLetter = button.getAttribute("data-letter");
+        console.log("Button clicked:", button.getAttribute("data-letter"));
         const isFilterActive = button.classList.contains("active");
-
-        letterButtons.forEach(otherButton => {
-          if (otherButton !== button) {
-            otherButton.classList.remove("active");
-          }
-        });
 
         if (isFilterActive) {
           button.classList.remove("active");
+          console.log("Filter is active. Removing filter...");
           displayResults(sortedData);
         } else {
+          console.log("Filter is not active. Applying filter...");
           button.classList.add("active");
           const filteredResults = filterResultsByLetter(selectedLetter, sortedData);
           displayResults(filteredResults);
         }
       });
     });
-
-    displayResults(sortedData);
-    searchInput.addEventListener("input", event => handleSearch(event, sortedData));
   })
   .catch(error => {
     console.error("Une erreur s'est produite lors du chargement des données.", error);
   });
 
-// Fonction pour filtrer les résultats par lettre
 function filterResultsByLetter(letter, data) {
   return data.filter(item =>
     item.abreviation.charAt(0).toLowerCase() === letter.toLowerCase()
