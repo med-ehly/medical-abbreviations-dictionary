18973c5340e8970d101df404229d872d8f2c06d4
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-08-18 09:35:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ function displaySearchResults(results) {
     resultsList.innerHTML = "<li>Aucun résultat trouvé</li>";
     return;
   }
-  
+
   results.forEach(result => {
     const row = document.createElement("li"); // Créer une ligne de tableau (élément li)
-    
+
     const abbrCell = document.createElement("abbr"); // Créer une cellule pour l'abréviation
     abbrCell.textContent = result.abreviation;
     row.appendChild(abbrCell); // Ajouter la cellule à la ligne
-    
+
     const descriptionCell = document.createElement("p"); // Créer une cellule pour la description
     descriptionCell.textContent = result.signification;
     row.appendChild(descriptionCell); // Ajouter la cellule à la ligne
-    
+
     resultsList.appendChild(row); // Ajouter la ligne au tableau de résultats
   });
 }
@@ -34,26 +34,25 @@ function handleSearch(event, data) {
 }
 
 function displayResults(results) {
-  console.log("Displaying results:", results);
-  const resultsList = document.getElementById("resultsList"); // Correction de l'ID
+  const resultsList = document.getElementById("resultsList");
   resultsList.innerHTML = '';
 
   if (results.length === 0) {
     resultsList.innerHTML = "<li>Aucun résultat trouvé</li>";
     return;
   }
-  
+
   results.forEach(result => {
     const row = document.createElement("li"); // Créer une ligne de tableau (élément li)
-    
+
     const abbrCell = document.createElement("abbr"); // Créer une cellule pour l'abréviation
     abbrCell.textContent = result.abreviation;
     row.appendChild(abbrCell); // Ajouter la cellule à la ligne
-    
+
     const descriptionCell = document.createElement("p"); // Créer une cellule pour la description
     descriptionCell.textContent = result.signification;
     row.appendChild(descriptionCell); // Ajouter la cellule à la ligne
-    
+
     resultsList.appendChild(row); // Ajouter la ligne au tableau de résultats
   });
 }
@@ -70,42 +69,41 @@ fetch("data.json")
     const letterButtons = document.querySelectorAll(".letter-button");
     let activeLetterButton = null;
 
-letterButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const selectedLetter = button.getAttribute("data-letter");
-    console.log("Button clicked:", button.getAttribute("data-letter"));
-    const isFilterActive = button.classList.contains("active");
+    letterButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        const selectedLetter = button.getAttribute("data-letter");
+        const isFilterActive = button.classList.contains("active");
 
-    if (activeLetterButton) {
-      activeLetterButton.classList.remove("active");
-      console.log("Removing active from previous letter:", activeLetterButton.getAttribute("data-letter"));
-    }
+        if (activeLetterButton) {
+          activeLetterButton.classList.remove("active");
+        }
 
-    if (!isFilterActive) {
-      console.log("Applying filter...");
-      button.classList.add("active");
-      activeLetterButton = button;
-    } else {
-      console.log("Removing filter...");
-      activeLetterButton = null; // Désactive la lettre active
-    }
+        if (!isFilterActive) {
+          button.classList.add("active");
+          activeLetterButton = button;
+        } else {
+          activeLetterButton = null;
+        }
 
-    // Si aucune lettre n'est active, affiche tous les résultats
-    if (!activeLetterButton) {
-      displayResults(sortedData);
-    } else {
-      const filteredResults = filterResultsByLetter(selectedLetter, sortedData);
-      displayResults(filteredResults);
-    }
-  });
-});
-     // Récupérer les catégories uniques et les types uniques
+        if (!activeLetterButton) {
+          displayResults(sortedData);
+        } else {
+          const filteredResults = filterResultsByLetter(selectedLetter, sortedData);
+          displayResults(filteredResults);
+        }
+      });
+    });
+
+    // Récupérer les catégories uniques et les types uniques
     const uniqueCategories = [...new Set(sortedData.map(item => item.categorie))];
     const uniqueTypes = [...new Set(sortedData.map(item => item.type))];
 
     // Générer les boutons de filtre pour les catégories
     const categoryFilterContainer = document.createElement("div");
-    categoryFilterContainer.classList.add("filter-container");
+    categoryFilterContainer.classList.add("category-filter");
+    const categoryFilterTitle = document.createElement("h2");
+    categoryFilterTitle.textContent = "Catégories";
+    categoryFilterContainer.appendChild(categoryFilterTitle);
 
     uniqueCategories.forEach(category => {
       const categoryButton = document.createElement("button");
@@ -114,13 +112,16 @@ letterButtons.forEach(button => {
       categoryButton.addEventListener("click", () => handleCategoryFilter(category, sortedData));
       categoryFilterContainer.appendChild(categoryButton);
     });
-    
-// Insérer les boutons de filtre de catégories avant les résultats
-    resultsList.parentNode.insertBefore(categoryFilterContainer, resultsList);
+
+    // Insérer les boutons de filtre de catégories avant les résultats
+    document.body.insertBefore(categoryFilterContainer, resultsList);
 
     // Générer les boutons de filtre pour les types de termes
     const typeFilterContainer = document.createElement("div");
-    typeFilterContainer.classList.add("filter-container");
+    typeFilterContainer.classList.add("type-filter");
+    const typeFilterTitle = document.createElement("h2");
+    typeFilterTitle.textContent = "Types";
+    typeFilterContainer.appendChild(typeFilterTitle);
 
     uniqueTypes.forEach(type => {
       const typeButton = document.createElement("button");
@@ -129,8 +130,9 @@ letterButtons.forEach(button => {
       typeButton.addEventListener("click", () => handleTypeFilter(type, sortedData));
       typeFilterContainer.appendChild(typeButton);
     });
-  // Insérer les boutons de filtre de types avant les résultats
-    resultsList.parentNode.insertBefore(typeFilterContainer, resultsList);
+
+    // Insérer les boutons de filtre de types avant les résultats
+    document.body.insertBefore(typeFilterContainer, resultsList);
   })
   .catch(error => {
     console.error("Une erreur s'est produite lors du chargement des données.", error);
@@ -140,4 +142,14 @@ function filterResultsByLetter(letter, data) {
   return data.filter(item =>
     item.abreviation.charAt(0).toLowerCase() === letter.toLowerCase()
   );
+}
+
+function handleCategoryFilter(category, data) {
+  const filteredResults = data.filter(item => item.categorie === category);
+  displayResults(filteredResults);
+}
+
+function handleTypeFilter(type, data) {
+  const filteredResults = data.filter(item => item.type === type);
+  displayResults(filteredResults);
 }
