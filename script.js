fdfa57dc723d9351d9e3f8db3ab479208858651c
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-08-18 09:41:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -99,40 +99,28 @@ fetch("data.json")
     const uniqueTypes = [...new Set(sortedData.map(item => item.type))];
 
     // Générer les boutons de filtre pour les catégories
-    const categoryFilterContainer = document.createElement("div");
-    categoryFilterContainer.classList.add("category-filter");
-    const categoryFilterTitle = document.createElement("h2");
-    categoryFilterTitle.textContent = "Catégories";
-    categoryFilterContainer.appendChild(categoryFilterTitle);
+    const categoryFilter = document.querySelector(".category-filter");
+    categoryFilter.innerHTML = "<h2>Catégories</h2>";
 
     uniqueCategories.forEach(category => {
       const categoryButton = document.createElement("button");
       categoryButton.textContent = category;
       categoryButton.classList.add("category-button");
       categoryButton.addEventListener("click", () => handleCategoryFilter(category, sortedData));
-      categoryFilterContainer.appendChild(categoryButton);
+      categoryFilter.appendChild(categoryButton);
     });
 
-    // Insérer les boutons de filtre de catégories avant les résultats
-    document.body.insertBefore(categoryFilterContainer, resultsList);
-
     // Générer les boutons de filtre pour les types de termes
-    const typeFilterContainer = document.createElement("div");
-    typeFilterContainer.classList.add("type-filter");
-    const typeFilterTitle = document.createElement("h2");
-    typeFilterTitle.textContent = "Types";
-    typeFilterContainer.appendChild(typeFilterTitle);
+    const typeFilter = document.querySelector(".type-filter");
+    typeFilter.innerHTML = "<h2>Types</h2>";
 
     uniqueTypes.forEach(type => {
       const typeButton = document.createElement("button");
       typeButton.textContent = type;
       typeButton.classList.add("type-button");
       typeButton.addEventListener("click", () => handleTypeFilter(type, sortedData));
-      typeFilterContainer.appendChild(typeButton);
+      typeFilter.appendChild(typeButton);
     });
-
-    // Insérer les boutons de filtre de types avant les résultats
-    document.body.insertBefore(typeFilterContainer, resultsList);
   })
   .catch(error => {
     console.error("Une erreur s'est produite lors du chargement des données.", error);
