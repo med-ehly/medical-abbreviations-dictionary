a054b29066cd70cc7d7c64f58c58ba5355fe0d19
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-08-23 22:47:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             const allCategories = ["Anesthésie", "Cardiologie", "CEGDC", "CCVT", "Dermatologie", "Endocrinologie", "Gastrologie", "Génétique", "Gériatrie", "Gynécologie", "Hémato-Onco", "Immuno-Allergie", "Med Interne", "Infectio", "Néphrologie", "Neurochirurgie", "Neurologie", "Ophtalmologie", "ORL", "Orthopédie", "Pédiatrie", "Physiatrie", "Plastie", "Pneumologie", "Psychiatrie", "Rhumatologie", "Urologie"];
-            const allTypes = ["Traitement", /* Ajoutez d'autres types */];
+            const allTypes = ["Traitement", "Diagnostic", "Symbole"];
 
             const categoryFilter = document.querySelector(".category-filter");
             categoryFilter.innerHTML = "<h2>Catégories</h2>";
