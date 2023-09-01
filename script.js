e10fad0821b6c5ac6f8a2831cd6358837026f36b
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-09-01 13:34:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -135,9 +135,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     handleLetterButtonClick(button);
                 });
             });
-
-            // Associez la fonction handleSymbolFilterButtonClick au clic sur le bouton de filtre "Symbole"
-            symbolFilterButton.addEventListener("click", handleSymbolFilterButtonClick);
             
             const allCategories = ["Anesthésie", "Cardiologie", "CEGDC", "CCVT", "Dermatologie", "Endocrinologie", "Gastrologie", "Génétique", "Gériatrie", "Gynécologie", "Hémato-Onco", "Immuno-Allergie", "Med Interne", "Infectio", "Néphrologie", "Neurochirurgie", "Neurologie", "Ophtalmologie", "ORL", "Orthopédie", "Pédiatrie", "Physiatrie", "Plastie", "Pneumologie", "Psychiatrie", "Rhumatologie", "Urologie"];
             const allTypes = ["Anatomie", "Diagnostic", "Examen", "Médication", "Traitement"];
