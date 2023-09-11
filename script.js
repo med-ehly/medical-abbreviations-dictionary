a336923a38759ad3f9f9ef20c9f35e4a3909f7bb
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-09-11 10:50:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -296,8 +296,8 @@ function handleMouseEnter(event) {
 
   // Calculer la position en fonction de l'élément cible
   const rowRect = row.getBoundingClientRect();
-  const top = rowRect.bottom + window.scrollY;
-  const left = rowRect.right + window.scrollX; 
+  const top = rowRect.bottom + window.scrollY - 5;
+  const left = rowRect.right + window.scrollX - 5; 
 
   // Appliquer la position calculée au popover
   popover.style.top = `${top}px`;
