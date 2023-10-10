1fded5f3d96218985b8b99d00eca6e83f8d2c1cb
med-ehly <142403355+med-ehly@users.noreply.github.com>
2023-10-10 15:22:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -293,9 +293,7 @@ function significationMatches(signification, searchTerm) {
 
 function displayResults(results){
    resultsList.innerHTML = '';
-
- 
-  if (results.length === 0) {
+if (results.length === 0) {
     resultsList.innerHTML = "<li>Aucun résultat trouvé</li>";
     return;
   }
@@ -335,54 +333,27 @@ function displayResults(results){
 
       // Ajoutez chaque résultat à la section
       groupResults.forEach(result => {
-        // Vérifiez si le résultat correspond au filtre actif
-        if (result.type === activeTypeFilter) {
-          const row = document.createElement("li");
-          const abbrCell = document.createElement("abbr");
-          abbrCell.textContent = result.abreviation;
-          row.appendChild(abbrCell);
+        const row = document.createElement("li");
+        const abbrCell = document.createElement("abbr");
+        abbrCell.textContent = result.abreviation;
+        row.appendChild(abbrCell);
 
-          // Create a container for the significations
-          const descriptionContainer = document.createElement("div");
-          descriptionContainer.classList.add("description-container");
+        // Create a container for the significations
+        const descriptionContainer = document.createElement("div");
+        descriptionContainer.classList.add("description-container");
 
-          // Check if there are multiple significations
-          if (result.significations && Array.isArray(result.significations)) {
-            // Add each signification and its icon to the container
-            result.significations.forEach(signification => {
-              const significationContainer = document.createElement("div");
-              significationContainer.classList.add("signification-container");
-
-              const descriptionText = document.createElement("p");
-              descriptionText.innerHTML = `➤ ${signification.signification}`;
-              significationContainer.appendChild(descriptionText);
-
-              if (signification.url) {
-                const icon = document.createElement("img");
-                icon.src = "monicone.svg";
-                icon.alt = "Lien externe";
-                icon.style.cursor = "pointer";
-                icon.classList.add("icon-class");
-
-                icon.addEventListener("click", () => {
-                  window.open(signification.url, "_blank");
-                });
-
-                significationContainer.appendChild(icon);
-              }
-
-              descriptionContainer.appendChild(significationContainer);
-            });
-          } else {
-            // If there's only one signification, display it along with its icon
+        // Check if there are multiple significations
+        if (result.significations && Array.isArray(result.significations)) {
+          // Add each signification and its icon to the container
+          result.significations.forEach(signification => {
             const significationContainer = document.createElement("div");
             significationContainer.classList.add("signification-container");
 
             const descriptionText = document.createElement("p");
-            descriptionText.innerHTML = `➤ ${result.signification}`;
+            descriptionText.innerHTML = `➤ ${signification.signification}`;
             significationContainer.appendChild(descriptionText);
 
-            if (result.url) {
+            if (signification.url) {
               const icon = document.createElement("img");
               icon.src = "monicone.svg";
               icon.alt = "Lien externe";
@@ -390,32 +361,56 @@ function displayResults(results){
               icon.classList.add("icon-class");
 
               icon.addEventListener("click", () => {
-                window.open(result.url, "_blank");
+                window.open(signification.url, "_blank");
               });
 
               significationContainer.appendChild(icon);
             }
 
             descriptionContainer.appendChild(significationContainer);
+          });
+        } else {
+          // If there's only one signification, display it along with its icon
+          const significationContainer = document.createElement("div");
+          significationContainer.classList.add("signification-container");
+
+          const descriptionText = document.createElement("p");
+          descriptionText.innerHTML = `➤ ${result.signification}`;
+          significationContainer.appendChild(descriptionText);
+
+          if (result.url) {
+            const icon = document.createElement("img");
+            icon.src = "monicone.svg";
+            icon.alt = "Lien externe";
+            icon.style.cursor = "pointer";
+            icon.classList.add("icon-class");
+
+            icon.addEventListener("click", () => {
+              window.open(result.url, "_blank");
+            });
+
+            significationContainer.appendChild(icon);
           }
 
-          // Add the descriptionContainer to the row
-          row.appendChild(descriptionContainer);
-
-          // Create a "langue popover" element for both single and multiple significations
-          const languePopover = document.createElement("div");
-          languePopover.classList.add("langue-popover");
-          languePopover.textContent = result.langue; // Récupérez la langue à partir des données JSON
-
-          // Add the languePopover to the row
-          row.appendChild(languePopover);
-
-          groupSection.appendChild(row);
-
-          // Ajoutez les gestionnaires d'événements au survol (mouseenter et mouseleave) pour chaque élément <li>
-          row.addEventListener('mouseenter', handleMouseEnter);
-          row.addEventListener('mouseleave', handleMouseLeave);
+          descriptionContainer.appendChild(significationContainer);
         }
+
+        // Add the descriptionContainer to the row
+        row.appendChild(descriptionContainer);
+
+        // Create a "langue popover" element for both single and multiple significations
+        const languePopover = document.createElement("div");
+        languePopover.classList.add("langue-popover");
+        languePopover.textContent = result.langue; // Récupérez la langue à partir des données JSON
+
+        // Add the languePopover to the row
+        row.appendChild(languePopover);
+
+        groupSection.appendChild(row);
+
+        // Ajoutez les gestionnaires d'événements au survol (mouseenter et mouseleave) pour chaque élément <li>
+        row.addEventListener('mouseenter', handleMouseEnter);
+        row.addEventListener('mouseleave', handleMouseLeave);
       });
 
       resultsList.appendChild(groupSection);
